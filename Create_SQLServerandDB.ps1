8ea2b00b74d74b3641bb717013a7c2806118aa90
hessiyad79 <45412612+hessiyad79@users.noreply.github.com>
2019-04-28 14:03:51 -0500

    Added ps file to cerate SQL server and SQL database

---
+++ Create_SQLServerandDB.ps1
@@ -0,0 +1,33 @@
+﻿#setup your account
+Login-AzureRmAccount
+
+#verify your subscriptions
+Get-AzureRmSubscription
+
+#set your default Subscription
+Get-AzureRmSubscription `
+–SubscriptionName “Visual Studio Premium with MSDN” | Select-AzureRmSubscription
+
+#create the Azure SQL server
+$adminCredential = Get-Credential
+
+New-AzureRmSqlServer -ResourceGroupName "hichamdemo" `
+-Location "East US 2" `
+-ServerName "hichamdemopshell" `
+-SqlAdministratorCredentials $adminCredential `
+-ServerVersion "12.0"
+
+#create a new Basic database
+New-AzureRmSqlDatabase `
+-DatabaseName "hichamdbpshell" `
+-ServerName "hichamdemopshell" `
+-ResourceGroupName "hichamdemo" `
+-Edition "Basic"
+
+# -- set a database level rule
+# EXEC sp_set_database_firewall_rule @name = N'ClientFirewallRule', 
+#     @start_ip_address = '99.230.128.115', @end_ip_address = '99.230.128.115'
+# 
+# -- query the rules
+# select * from sys.firewall_rules
+# select * from sys.database_firewall_rules
